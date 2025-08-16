3b3b84e9bf803342198748b431d4916513cd2f9a
Intergalactyc <78564099+Intergalactyc@users.noreply.github.com>
2025-08-16 11:01:12 -0500

    Black hook

---
+++ windprofiles/quantities/variables.py
@@ -0,0 +1,16 @@
+from windprofiles.quantities.structures import Dimension, Variable
+
+_v_temperature = Variable(
+    "Temperature",
+    Dimension.Temperature,
+    ["t", "temp", "tmpr"],
+)
+_v_dewpoint = Variable("Dewpoint", Dimension.Temperature, ["dwpt", "dew", "tdew"])
+_v_relative_humidity = Variable(
+    "Relative Humidity",
+    Dimension.Dimless,
+    ["rh", "rhum", "relh", "relative_humidity"],
+)
+_v_wind_direction = Variable(
+    "Wind Direction", Dimension.Angle, ["wd", "wdir", "dir"]
+)
