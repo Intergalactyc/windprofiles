c4c0ff455de3907dc39f0f3823696f3090f36a80
Intergalactyc <22walkerelliott@gmail.com>
2025-08-23 12:54:12 -0500

    Timestamping for TimeSeries

--- windprofiles/structures/timeseries.py
+++ windprofiles/structures/timeseries.py
@@ -25,10 +25,12 @@ class TimeSeries:
         `timezone` should be the timezone of the datetime column
 
         If the datetime column is not of a common format, pass a formatter string with `datetime_format`
+        (see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior)
 
-        If no datetime column exists in `data`, then a start time `start_time` and frequency `freq`
-        (see https://pandas.pydata.org/docs/user_guide/timeseries.html#timeseries-offset-aliases)
-        must be provided
+        If no datetime column ("time", "timestamp", or "datetime") exists in `data`, then a start time
+        `start_time` and frequency `freq` must be provided
+        (see https://pandas.pydata.org/docs/user_guide/timeseries.html#timeseries-offset-aliases; actual
+        frequencies may also be given in Hz)
         """
         self._warnings = []
         self._df = data.copy()
@@ -75,15 +77,57 @@ class TimeSeries:
                 raise TypeError(
                     "Explicit timestamp column not present in data - must provide a start_time and freq"
                 )
+            if isinstance(freq, int):
+                freq = TimeSeries._hz_to_freq_str(freq)
+            elif isinstance(freq, str) and freq.lower().endswith("hz"):
+                freq = TimeSeries._hz_to_freq_str(float(freq[:-2]))
             self._df.index = pd.date_range(
-                start_time,
+                start_time, periods=len(self._df), freq=freq
             )
+
+        if tz := self._df.tz:
+            if tz != self._tz:
+                self._warnings.append(
+                    f"Found timezone {tz} in data which may be different than passed timezone {self._tz}"
+                )
+        else:
+            self._df.tz_localize(self._tz)
+
+    @staticmethod
+    def _hz_to_freq_str(f_hz: int | float):
+        return f"{int(1e9/f_hz)}ns"
+
+    @property
+    def timedelta(self) -> pd.Timedelta:
+        """Get the characteristic timestep between timestamps"""
+        if f := self._df.freq:
+            """If there is already an explicit frequency, use that"""
+            return pd.to_timedelta(f)
+        else:
+            """Otherwise, use the mode timestep across full datetime index"""
+            return self._df.to_series().diff().value_counts().idxmax()
+
+    @property
+    def frequency(self) -> float:
+        """Get timestamp frequency in Hz"""
+        return 1.0 / (
+            self.timedelta.hour * 3600
+            + self.timedelta.minute * 60
+            + self.timedelta.microseconds * 1e-6
+            + self.timedelta.nanoseconds * 1e-9
+        )
 
     def _df_converted(self, to_units: dict):
         out = self._df.copy()
         for column in out.columns:
-            var = self._columns_to_variables[column]
-            self._variables.get
+            if column in to_units:
+                var = self._columns_to_variables[column]
+                out[column] = var.convert(out[column], to_units[column])
+            else:
+                self._warnings.append(
+                    f"When converting to units '{to_units}', no unit found for {column} (using default)"
+                )
+        return out
 
     @property
     def warnings(self):
