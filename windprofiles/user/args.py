d76d4859f918a46376857de978c641025ffd65f9
Intergalactyc <22walkerelliott@gmail.com>
2025-11-03 15:51:09 -0600

    Minor sonic updates

--- windprofiles/user/args.py
+++ windprofiles/user/args.py
@@ -3,19 +3,26 @@ import argparse
 
 
 class Parser:
-    def __init__(self, paths: list = [], define: list = []):
-        if set(paths) & set(define):
+    def __init__(
+        self, paths: list = [], define: list = [], special: list = []
+    ):
+        if (
+            set(paths) & set(define)
+            or set(paths) & set(special)
+            or set(define) & set(special)
+        ):
             raise ValueError(
-                "Cannot have overlap between `paths` and `define`"
+                "Cannot have any overlap between `paths`, `define`, `special"
             )
 
         self._paths = paths
         self._define = define
+        self._special = special
 
         self.argparser = argparse.ArgumentParser()
         self.argparser.add_argument("--config", type=str, default="config.ini")
 
-        self.cfgparser = configparser.ConfigParser()
+        self.cfgparser = configparser.ConfigParser(allow_no_value=True)
 
     def _parse_cl(self):
         args = self.argparser.parse_args()
@@ -31,6 +38,18 @@ class Parser:
         for s in self._define:
             sub = {k: v for k, v in self.cfgparser.items(s)}
             result[s] = sub
+        other_sections = {}
+        for s in self.cfgparser.sections():
+            if s == "paths" or s in self._define:
+                continue
+            sub = []
+            for k, v in self.cfgparser.items(s):
+                if k in self._special:
+                    result[k] = v
+                else:
+                    sub.append(k)
+            other_sections[s] = sub
+        result["other"] = other_sections
         return result
 
     def add_argument(self, *args, **kwargs):
